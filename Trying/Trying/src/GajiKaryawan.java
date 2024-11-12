cd05a2b019c68b9d812b6924045bed1173e99060
Faiznation <novaatalagrab@gmail.com>
2024-11-12 20:06:29 +0700

    just commit

--- Trying/Trying/src/GajiKaryawan.java
+++ Trying/Trying/src/GajiKaryawan.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class GajiKaryawan{
     public static void main(String[] args) {
         
-        System.out.println(null);
+   
         System.out.println(">>>Aplikasi Penghitung Gaji<<<");
         try (Scanner scanner = new Scanner(System.in)) {
             System.out.print("Enter Your Name: ");
--- Trying/Trying/src/Ubah.java
+++ Trying/Trying/src/Ubah.java
@@ -1,16 +1,28 @@
+import java.util.Scanner;
+
 public class Ubah {
-    
     public static void main(String[] args){
-        //Nilai Default pada Variable String
-        String smk = "smk plus ashabulyamin";
-        String web = "WILDAN TECHNO ART";
+        try (Scanner scanner = new Scanner(System.in)) {
+            //Nilai Default pada Variable String
+            String smk = "smk plus ashabulyamin";
+            String web = "WILDAN TECHNO ART";
 
-        //Nilai Yang DImanipulasi Menjadi Kapital dan Kecil
-        String UpSMK = smk.toUpperCase();
-        String DownWEB = web.toLowerCase();
+            //Nilai Yang DImanipulasi Menjadi Kapital dan Kecil
+            String UpSMK = smk.toUpperCase();
+            String DownWEB = web.toLowerCase();
 
-        //Mencetak Hasil
-        System.out.println(UpSMK);
-        System.out.println(DownWEB);
+            //Mencetak Hasil
+            System.out.println(UpSMK);
+            System.out.println(DownWEB);
+
+            System.out.println("=================================================");
+
+            System.out.print("Input nama: ");
+            String namaLengkap = scanner.nextLine();
+
+            String namaBelakang = namaLengkap.substring(3, 12);
+            System.out.println(namaBelakang); 
+            // output : Izzatul
+        }
     }
 }
